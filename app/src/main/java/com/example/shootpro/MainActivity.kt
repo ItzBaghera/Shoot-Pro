81bc1398cb2540f65a3da3b7f8e031f1f8b83024
Fortu <fortunato.2007pro@gmail.com>
2024-12-21 22:34:08 +0100

    Moved sideNav to bottomNav removed ripple effect

--- app/src/main/java/com/example/shootpro/MainActivity.kt
+++ app/src/main/java/com/example/shootpro/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.shootpro
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,8 +12,6 @@ import com.example.shootpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -27,9 +23,6 @@ class MainActivity : AppCompatActivity() {
         // Set up the toolbar
         setSupportActionBar(binding.toolbar)
 
-        // Initialize drawer layout
-        drawerLayout = binding.drawerLayout
-
         // Initialize NavController
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -37,28 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         // Set up app bar configuration
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow),
-            drawerLayout
+            setOf(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_cup, R.id.nav_profile)
         )
 
         // Set up Action Bar with NavController
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        // Set up Navigation View with NavController
+        // Set up Bottom Navigation View with NavController
         binding.navView.setupWithNavController(navController)
-
-        // Create the toggle for the drawer
-        toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            binding.toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-
-        // Add the toggle to the drawer
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
     }
 
     override fun onSupportNavigateUp(): Boolean {
